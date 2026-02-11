3a6327a892b49623fbf26223067a67b6f84c69e6
koshachya-myatka <annaparamonova18122004@gmail.com>
2026-02-11 11:43:36 +0400

    updated add task

--- Distributor/demo/src/main/java/ru/paramonova/DistributorApplication.java
+++ Distributor/demo/src/main/java/ru/paramonova/DistributorApplication.java
@@ -11,21 +11,9 @@ public class DistributorApplication {
     // комп
     // java -jar "D:\Study\Grid Calculations\Distributor\demo\target\Distributor-0.jar" "D:\Study\Grid Calculations\test2.txt"
     // ноут
-    // java -jar "C:\Study\Grid Calculations\Distributor\demo\target\Distributor-0.jar" "C:\Study\Grid Calculations\test2.txt"
-
-    @Bean
-    public Distributor distributor() {
-        String fileName = System.getProperty("fileName");
-        System.out.println("Используется файл: " + fileName);
-        return new Distributor(fileName);
-    }
+    // java -jar "C:\Study\grid-calculations\Distributor\demo\target\Distributor-0.jar" "C:\Study\grid-calculations\test2.txt"
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("Укажите название файла для задачи");
-            System.exit(1);
-        }
-        System.setProperty("fileName", args[0]);
         SpringApplication.run(DistributorApplication.class, args);
     }
 }
--- Distributor/demo/src/main/java/ru/paramonova/controllers/DistributorController.java
+++ Distributor/demo/src/main/java/ru/paramonova/controllers/DistributorController.java
@@ -1,22 +1,38 @@
 package ru.paramonova.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.paramonova.models.Distributor;
+import ru.paramonova.models.Pipe;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class DistributorController {
 
     private final Distributor distributor;
+    private int index = 0;
 
-    @GetMapping("/info")
-    public String getFieldInfo() {
-        return String.format("Ширина: %.0f,\nВысота: %.0f,\nКоличество кругов: %d,\nКруги: %s",
-                distributor.getFieldWidth(),
-                distributor.getFieldLength(),
-                distributor.getCircles().size(),
-                distributor.getCircles());
+    @PostMapping("/add/{path:.+}")
+    public ResponseEntity<Integer> addTask(@PathVariable("path") String path) {
+        distributor.addTask(path);
+        return ResponseEntity.ok(distributor.getLastTaskId());
+    }
+
+    @GetMapping("/task/{taskId}/info")
+    public ResponseEntity<String> getFieldInfo(@PathVariable("taskId") int taskId) {
+        return ResponseEntity.ok(String.format("Ширина: %.0f,\nВысота: %.0f,\nКоличество кругов: %d,\nКруги: %s",
+                distributor.getTask(taskId).getFieldWidth(),
+                distributor.getTask(taskId).getFieldLength(),
+                distributor.getTask(taskId).getCircles().size(),
+                distributor.getTask(taskId).getCircles()));
+    }
+
+    @GetMapping("/task/{taskId}")
+    public ResponseEntity<String> getCombination(@PathVariable("taskId") int taskId) {
+        return ResponseEntity.ok(distributor.getTask(taskId)
+                .getCombinations().get(index++).toString());
     }
 }
